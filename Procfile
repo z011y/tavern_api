d38c27f11100814f819d4001ae2934c8410fb36e
cameron-charles <cameron.ntc@gmail.com>
2020-02-04 21:54:58 -0700

    migrated from mysql to postgresql database

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tavern_api.wsgi
--- tavern_api/settings.py
+++ tavern_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import datetime
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -81,12 +83,12 @@ WSGI_APPLICATION = 'tavern_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'tavern_users',
-        'USER': 'root',
-        'PASSWORD': 'NEWPASSWORD',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tavern_db',
+        'USER': 'cameronzollinger',
+        'PASSWORD': 'Santorini17',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
 
@@ -160,3 +162,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
